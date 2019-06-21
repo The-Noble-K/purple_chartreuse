004a5b58fbe508fcd09c314fac331ea846721533
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-06-21 18:22:56 -0400

    header, logo, paragraph

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Helmet } from 'react-helmet'
+import logo from './images/TransparentBlackLogo.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Helmet bodyAttributes={{style: 'background-color : #88569d'}} />
+
+        <div className='header'>
+          <h1>Purple Chartreuse</h1>
+        </div>
+          
+        <div className='logo-container'>
+          <img className='logo' src={logo} alt='Purple Chartreuse logo' />
+        </div>
+    
+        <div className='bio'>
+          <p>
+            Purple Chartreuse is made up of members Jarod Sneed and Kevin Teachey. We began as roommates, where the idea
+            behind Purple Chartreuse was formed through our mutual love of music production. Purple Chartruese, to us, is a blend
+            of musical genres, nifty ideas, crazy thoughts and intense feelings into sonic vibration. 
+            Our music is made with instruments both digital, physical and analogue. We are proud to boast this fusion of elements
+            varying from analogue to digital synthesizers, to guitars, piano, bass and even a didgeridoo. 
+          </p>
+        </div>
+    
+        <div className='links'>
+    
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
